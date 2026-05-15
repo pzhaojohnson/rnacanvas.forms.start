52908a96d771731eb3bc05ec0adc3a8dc0c2817c
Philip Johnson <p.zhao.johnson@gmail.com>
2026-05-15 18:52:13 -0400

    Add Start page class component

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
  * @type { import('jest').Config }
  */
 const config = {
+  "moduleNameMapper": {
+    "\\.css$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.svg$": "<rootDir>/__mocks__/fileMock.js",
+  },
   "transformIgnorePatterns": [
     "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)",
   ],
